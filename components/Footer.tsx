44c8c70cad89147ecc0631c16f33b0cac6f1a9e0
Snigdha Singh <snigdha0903singh@gmail.com>
2025-06-07 14:52:17 +0530

    second-commit

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="mt-auto py-6 text-center text-sm text-gray-500">
-      &copy; {new Date().getFullYear()} Snigdha Singh. All rights reserved.
-    </footer>
-  );
-}
